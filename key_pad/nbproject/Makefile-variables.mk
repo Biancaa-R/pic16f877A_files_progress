272ef8a57886783c977bcd5ae822842ed9c2e165
Biancaa Ramesh <87770461+Biancaa-R@users.noreply.github.com>
2025-01-29 14:24:59 +0000

    Add files via upload

---
+++ key_pad/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=key_pad.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/key_pad.production.hex
